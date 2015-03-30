101803e32a67d8f9de4791e60d9ed988501fdbf9
KatoriKai <toobad.tm@gmail.com>
2015-03-31 07:56:30 +0800

    ステータスの調整

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemViewModel.cs
@@ -14,7 +14,7 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
     {
         public ShipSlot Slot { get; set; }
         public SlotItemInfo Info { get; set; }
-        
+
         public SlotItemViewModel()
         {
         }
@@ -31,39 +31,33 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
             }
         }
 
+        private string SignedStr(int Value)
+		{
+			return Value.ToString("+#;-#;#");
+		}
+
         public string Detail
         {
             get
-            {
-                List<string> details = new List<string>();
+			{
+                string AddDetail = "";
 
-                if (this.Info.Firepower != 0) details.Add(StatFormat(this.Info.Firepower, Resources.Stats_Firepower));
-                if (this.Info.AA != 0) details.Add(StatFormat(this.Info.AA, Resources.Stats_AntiAir));
-                if (this.Info.Torpedo != 0) details.Add(StatFormat(this.Info.Torpedo, Resources.Stats_Torpedo));
-                if (this.Info.AntiSub != 0) details.Add(StatFormat(this.Info.AntiSub, Resources.Stats_AntiSub));
-                if (this.Info.SightRange != 0) details.Add(StatFormat(this.Info.SightRange, Resources.Stats_SightRange));
-                if (this.Info.Speed != 0) details.Add(StatFormat(this.Info.Speed, Resources.Stats_Speed));
-                if (this.Info.Armor != 0) details.Add(StatFormat(this.Info.Armor, Resources.Stats_Armor));
-                if (this.Info.Health != 0) details.Add(StatFormat(this.Info.Health, Resources.Stats_Health));
-                if (this.Info.Luck != 0) details.Add(StatFormat(this.Info.Luck, Resources.Stats_Luck));
-                if (this.Info.Evasion != 0) details.Add(StatFormat(this.Info.Evasion, Resources.Stats_Evasion));
-                if (this.Info.Accuracy != 0) details.Add(StatFormat(this.Info.Accuracy, Resources.Stats_Accuracy));
-                if (this.Info.DiveBomb != 0) details.Add(StatFormat(this.Info.DiveBomb, Resources.Stats_DiveBomb));
-                if (this.Info.AttackRange > 0) details.Add(String.Format(" {1}({0})", this.Info.AttackRange, Resources.Stats_AttackRange));
-                //if (this.Info.RawData.api_raik > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Info.RawData.api_raik + " api_raik";
-                //if (this.Info.RawData.api_raim > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Info.RawData.api_raim + " api_raim";
-                //if (this.Info.RawData.api_sakb > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Info.RawData.api_sakb + " api_sakb";
-                //if (this.Info.RawData.api_atap > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Info.RawData.api_atap + " api_atap";
-                //if (this.Info.RawData.api_rare > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Info.RawData.api_rare + " api_rare";
-                //if (this.Info.RawData.api_bakk > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Info.RawData.api_bakk + " api_bakk";
-
-                return String.Join("\n", details);
+                if (this.Info.Firepower != 0) AddDetail += Resources.Stats_Firepower + " " + SignedStr(this.Info.Firepower);
+                if (this.Info.AA != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_AntiAir + " " + SignedStr(this.Info.AA);
+                if (this.Info.Torpedo != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Torpedo + " " + SignedStr(this.Info.Torpedo);
+                if (this.Info.AntiSub != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_AntiSub + " " + SignedStr(this.Info.AntiSub);
+                if (this.Info.SightRange != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_SightRange + " " + SignedStr(this.Info.SightRange);
+                if (this.Info.Speed != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Speed + " " + SignedStr(this.Info.Speed);
+                if (this.Info.Armor != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Armor + " " + SignedStr(this.Info.Armor);
+                if (this.Info.Health != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Health + " " + SignedStr(this.Info.Health);
+                if (this.Info.Luck != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Luck + " " + SignedStr(this.Info.Luck);
+                if (this.Info.Accuracy != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Accuracy + " " + SignedStr(this.Info.Accuracy);
+                if (this.Info.Evasion != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_Evasion + " " + SignedStr(this.Info.Evasion);
+                if (this.Info.DiveBomb != 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_DiveBomb + " " + SignedStr(this.Info.DiveBomb);
+                if (this.Info.AttackRange > 0) AddDetail += (AddDetail != "" ? "\n" : "") + Resources.Stats_AttackRange + " (" + this.Info.AttackRange + ")";
+                
+                return AddDetail;
             }
-        }
-
-        private string StatFormat(int stat, string name)
-        {
-            return String.Format(" {0:+#;-#} {1}", stat, name);
         }
     }
 }
